7f371799cc741c917f446f0b22127072bc5434a9
Grzesikk <74188778+Grzesikk@users.noreply.github.com>
2020-11-15 15:41:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title>Grzesikowski Mateusz</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+</head>
+
+<body>
+<br>
+<b>Zaloguj się:</b><br><br>
+<form method="post" action="zaloguj.php">
+ Login:<input type="text" name="user"><br><br></br>
+ Haslo:<input type="password" name="pass"><br><br>
+ <input type="submit" value="Zaloguj!"/>
+</form>
+<a href="rejestruj.php">Nie masz konta? Rejestracja!</a></p>
+
+</body>
+</html>
+</html>
